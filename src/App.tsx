3470f73302a121716d260dd60f5bc233e94a9097
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-08 20:56:14 +0200

    Add winner detection
    + outsource values

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Players } from './components/Players';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 
+import { WINNING_COMBINATIONS } from './winning-combinations';
+import { initialGameBoard } from './gameboard';
 
 import './App.scss'
 
@@ -27,6 +29,35 @@ const initialPlayers: [PlayerType, PlayerType] = [
 function App() {
   const [playersStats, setPlayersStats] = useState(initialPlayers);
   const [gameTurns, setGameTurns] = useState<string[]>([]);
+
+  const gameBoardLayout = initialGameBoard;
+  let winnerSymbol;
+  let winnerPlayer;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const {xPos, yPos } = square;
+    const { symbol } = player;
+    
+    gameBoardLayout[yPos][xPos].value = symbol;
+  }
+
+  for (const winningCombination of WINNING_COMBINATIONS) {
+
+    const firstSquareSymbol = initialGameBoard[winningCombination[0].row][winningCombination[0].column].value;
+    const secondSquareSymbol = initialGameBoard[winningCombination[1].row][winningCombination[1].column].value;
+    const thirdSquareSymbol = initialGameBoard[winningCombination[2].row][winningCombination[2].column].value; 
+    
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winnerSymbol = firstSquareSymbol;
+
+      winnerPlayer = playersStats.find((player) => {
+        if (player.symbol === winnerSymbol) {
+          return player;
+        }
+      })
+    }
+  }
 
   const handleEditPlayer = (e, playerId) => {
 
@@ -89,9 +120,10 @@ function App() {
       <div id="game-container">
         <Players players={playersStats} onEditPlayer={handleEditPlayer} />
 
+        { winnerPlayer && `You won, ${winnerPlayer.name}!`}
         <GameBoard
+          gameBoardLayout={gameBoardLayout}
           onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
           players={playersStats}
         />
       </div>
---
+++ src/gameboard.ts
@@ -0,0 +1,79 @@
+type GameBoard = {
+  id: number;
+  value: string | null;
+  isActive: boolean;
+  xPos: number;
+  yPos: number;
+}
+
+export const initialGameBoard : GameBoard[][] = [
+  [
+    {
+      id: 1,
+      value: null,
+      isActive: false,
+      xPos: 0,
+      yPos: 0,
+    },
+    {
+      id: 2,
+      value: null,
+      isActive: false,
+      xPos: 1,
+      yPos: 0,
+    },
+    {
+      id: 3,
+      value: null,
+      isActive: false,
+      xPos: 2,
+      yPos: 0,
+    }
+  ],
+  [
+    {
+      id: 4,
+      value: null,
+      isActive: false,
+      xPos: 0,
+      yPos: 1,
+    },
+    {
+      id: 5,
+      value: null,
+      isActive: false,
+      xPos: 1,
+      yPos: 1,
+    },
+    {
+      id: 6,
+      value: null,
+      isActive: false,
+      xPos: 2,
+      yPos: 1,
+    }
+  ],
+  [
+    {
+      id: 7,
+      value: null,
+      isActive: false,
+      xPos: 0,
+      yPos: 2,
+    },
+    {
+      id: 8,
+      value: null,
+      isActive: false,
+      xPos: 1,
+      yPos: 2,
+    },
+    {
+      id: 9,
+      value: null,
+      isActive: false,
+      xPos: 2,
+      yPos: 2,
+    }
+  ]
+];
